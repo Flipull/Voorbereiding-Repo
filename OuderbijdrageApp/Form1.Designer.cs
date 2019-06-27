709eddbaadd799e91af95186e8e1cf3cdc2aba0a
Flipull <52195724+Flipull@users.noreply.github.com>
2019-06-27 11:05:57 +0200

    First Commit

---
+++ OuderbijdrageApp/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace OuderbijdrageApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 51);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(198, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.DateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Peil Datum";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(16, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(274, 160);
+            this.listBox1.TabIndex = 2;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(16, 139);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(193, 20);
+            this.dateTimePicker2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Kinderen Geboortedata";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Toevoegen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Totale Ouderbijdrage: € 50,-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 385);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OuderbijdrageApp/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OuderbijdrageApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker1.Value = DateTime.Now;
+            dateTimePicker2.MaxDate = dateTimePicker1.Value;
+        }
+
+        //
+        private void CalculateOuderbijdrage()
+        {
+            DateTime peildatum = dateTimePicker1.Value;
+            TimeSpan tien_jaar_tijd = TimeSpan.FromDays(10 * 365);
+
+            byte kinder_aantal = 0, kinderen_tien_jaar_of_ouder = 0;
+            foreach (DateTime i in listBox1.Items)
+            {
+                kinder_aantal++;
+                TimeSpan tijdverschil = i.Subtract(peildatum);
+                if (peildatum.Subtract(i) >= tien_jaar_tijd)
+                {
+                    kinderen_tien_jaar_of_ouder++;
+                }
+            }
+
+            float Ouderbijdrage = 50 + 25 * kinder_aantal + 12 * kinderen_tien_jaar_of_ouder;
+
+            if (kinder_aantal == 1)
+                Ouderbijdrage =  Ouderbijdrage * (3 / 4f);//25% korting
+
+            label3.Text = "Totale Ouderbijdrage: € " + Ouderbijdrage;
+        }
+        //
+
+        private void DateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker2.MaxDate = dateTimePicker1.Value;
+            dateTimePicker2.Value = dateTimePicker1.Value;
+            listBox1.Items.Clear();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(dateTimePicker2.Value);
+
+            CalculateOuderbijdrage();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            CalculateOuderbijdrage();
+        }
+    }
+}
